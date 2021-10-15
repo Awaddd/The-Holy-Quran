6dda9e14d27f0fa52b7e101745eaf7165cb88107
Awad <awad@moblox.com>
2021-10-15 17:53:14 +0100

    Now calling the new API on button press

--- src/Components/Chapters/Chapters.tsx
+++ src/Components/Chapters/Chapters.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useQuery } from 'react-query'
-import { getAllChapters } from '../../Services/ChaptersService'
+import { getAllChapters, getChapterAudio } from '../../Services/ChaptersService'
 import Panel from '../../Library/Components/Panel/Panel'
 import Section from '../../Library/Components/Section/Section'
 
@@ -8,28 +8,32 @@ export interface ChaptersProps {
 
 }
 
-const playAudio = (chapterID: number) => {
-}
-
 function Chapters({ }: ChaptersProps) {
 
-    const { data, error, status } = useQuery('chapters', async () => getAllChapters())
+    //
+    const [chapterID, setChapterID] = useState<number|null>(null)
 
-    if (typeof data == 'undefined') {
-        return (
-            <p>No Chapters to display</p>
-        )
+    //
+    const { data: chapterAudio } = useQuery(["chapterAudio", { chapterID }], getChapterAudio)
+
+    //
+    useEffect(() => console.log(chapterAudio), [chapterAudio])
+
+    //
+    const { data: chapters } = useQuery('chapters', async () => getAllChapters())
+
+    // skip the component entirely if there is no data - in future show a skeletion with a loading indicator instead
+    if (typeof chapters == 'undefined') {
+        return ( <> </> )
     }
-
-    const chapters = data.chapters
 
     return (
         <Section classes="text-center">
             <h2 className="mb-8 sm:mb-12 text-4xl font-extrabold">Surah<span className="text-accent">'s</span></h2>
             <main className="sm:mt-3 sm:mx-auto sm:w-4/5 md:w-3/5 lg:w-2/5">
-                {chapters.map((chapter: any, index: number) => {
+                {chapters.chapters.map((chapter: any, index: number) => {
                     return (
-                        <Panel name={chapter['name_simple']} description={chapter['translated_name'].name} icon="play" action={() => playAudio(chapter['id'])}></Panel>
+                        <Panel name={chapter['name_simple']} description={chapter['translated_name'].name} icon="play" action={() => setChapterID(chapter['id'])}></Panel>
                     )
                 })}
             </main>
