f7a065b200669702937ec39c1911302a5a4c1370
Awad <awad@moblox.com>
2021-10-16 00:55:23 +0100

    Removed link from the home hero

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,7 +4,6 @@ import Chapters from '../Components/Chapters/Chapters'
 import Button from '../Library/Components/Button/Button'
 import Hero from '../Library/Components/Hero/Hero'
 import Reciters from '../Components/Reciters/Reciters'
-import Link from '../Library/Components/Link/Link'
 
 function Home() {
 
@@ -18,11 +17,10 @@ function Home() {
     const title = <h1 className="mb-5 text-5xl font-extrabold">The Holy <span className="text-primary underline">Qur'an</span></h1>
     const content = <p className="mb-5">Listen to your favourite Qur'an recitation at your convenience</p>
     const button = <Button classes="btn-primary" label='Get Started' action={scrollToTarget} />
-    const link = <Link label="Start Now" url="" />
 
     return (
         <main>
-            <Hero title={title} content={content} action={link}></Hero>
+            <Hero title={title} content={content} action={button}></Hero>
             <Reciters></Reciters>
             <Chapters></Chapters>
         </main>
