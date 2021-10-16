9a17b0dcc8eed048b0743654468a5d2cf69b1540
Awad <awad@moblox.com>
2021-10-16 22:44:02 +0100

    Now fetching Qur'an audio from a different source

--- src/Services/Api.ts
+++ src/Services/Api.ts
@@ -1,3 +1,4 @@
 // api config
 export const mainAPI = 'https://api.quran.com/api/v4'
 export const audioAPI = 'https://api.quran.sutanlab.id'
+export const audioSource = 'https://download.quranicaudio.com/quran'
--- src/Services/ChaptersService.ts
+++ src/Services/ChaptersService.ts
@@ -1,12 +1,10 @@
-import { mainAPI, audioAPI } from './Api'
+import { mainAPI, audioSource } from './Api'
 
 export const getAllChapters = async () => {
     return (await fetch(`${mainAPI}/chapters?language=en`)).json() 
 }
 
-export const getChapterAudio = async ({ queryKey }: any) => {
-    const [_key, { chapterID }] = queryKey
-
-    if (chapterID == null) return;
-    return (await fetch(`${audioAPI}/surah/${chapterID}`)).json()
+export const getChapterAudio = (id: string = '001') => {
+    const reciter: string = 'mishaari_raashid_al_3afaasee'
+    return (`${audioSource}/${reciter}/${id}.mp3`)
 }
