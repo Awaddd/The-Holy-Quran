05069c32bf1a1b764891f0c9ef6fc389ffb5f785
Awad <awad@moblox.com>
2021-10-16 01:35:10 +0100

    Storing chapters in global state using recoil

--- src/Components/Chapters/Chapters.tsx
+++ src/Components/Chapters/Chapters.tsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useQuery } from 'react-query'
 import { getAllChapters } from '../../Services/ChaptersService'
+import { useRecoilState } from 'recoil'
+import { chaptersState } from '../../State/state'
 import Section from '../../Library/Components/Section/Section'
 import Chapter from '../Chapter/Chapter'
 
@@ -11,20 +13,23 @@ export interface ChaptersProps {
 function Chapters({ }: ChaptersProps) {
 
     //
-    const { data: chapters } = useQuery('chapters', async () => getAllChapters())
+    const { data } = useQuery('chapters', async () => getAllChapters())
+    const [chapters, setChapters] = useRecoilState(chaptersState)
+
+    useEffect(() => {
+        if (data != null) setChapters(data.chapters)
+    }, [data])
 
     // skip the component entirely if there is no data - in future show a skeletion with a loading indicator instead
-    if (typeof chapters == 'undefined') {
+    if (typeof chapters == 'undefined' || chapters == null || chapters == []) {
         return ( <> </> )
     }
-
-    const control = 'play'
 
     return (
         <Section classes="text-center">
             <h2 className="mb-8 sm:mb-12 text-4xl font-extrabold">Surah<span className="text-accent">'s</span></h2>
             <main className="sm:mt-3 sm:mx-auto sm:w-4/5 md:w-3/5 lg:w-2/5">
-                {chapters.chapters.map((chapter: any) => {
+                {chapters.map((chapter: any) => {
                     return (
                         <Chapter key={chapter['id']} chapter={chapter} />
                     )
---
+++ src/State/state.ts
@@ -0,0 +1,5 @@
+import { atom, selector } from 'recoil';
+
+// global state
+export const chaptersState = atom({key: 'chapters', default: []})
+
