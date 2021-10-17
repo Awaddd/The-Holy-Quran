e8506dfcf03e06ddd86cc83868e215dd74f216a9
Awad <awad@moblox.com>
2021-10-17 22:50:11 +0100

    Now able to pause and play from the global player

--- src/Components/Player/Player.tsx
+++ src/Components/Player/Player.tsx
@@ -1,5 +1,7 @@
-import React from "react"
+import React, { useEffect } from "react"
 import ButtonIcon from "../../Library/Components/ButtonIcon/ButtonIcon"
+import { activeTrackState } from "../../State/state"
+import { useRecoilState } from "recoil"
 
 export interface PlayerProps {
 
@@ -7,13 +9,18 @@ export interface PlayerProps {
 
 function Player() {
 
-    let dynamic = 'play'
+    const [activeTrack, setActiveTrack] = useRecoilState(activeTrackState)
+
+    const controlActiveTrackPlayingState = () => {
+        if (activeTrack.hasOwnProperty('id') == false || activeTrack.id == null) return
+        setActiveTrack(activeTrack => { return {...activeTrack, isPlaying: !activeTrack.isPlaying} })
+    }
 
     return (
         <nav className="navbar bg-primary grid">
             <div className="grid grid-flow-col gap-2 justify-center">
                 <ButtonIcon icon="stop" action={() => alert('connected')}></ButtonIcon>
-                <ButtonIcon icon={dynamic} action={() => alert('connected')}></ButtonIcon>
+                <ButtonIcon icon={activeTrack.isPlaying === true ? 'pause' : 'play'} action={controlActiveTrackPlayingState}></ButtonIcon>
             </div>
         </nav>
     )
