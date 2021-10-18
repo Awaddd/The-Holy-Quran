f10aed3a1c379e1300c0aa84118f7244d9a0f33e
Awad <awad@moblox.com>
2021-10-18 12:11:22 +0100

    Ensured local images are used for reciters
    Also ensured images maintain their aspect ratio

--- src/Components/Reciter/Reciter.tsx
+++ src/Components/Reciter/Reciter.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-
 export interface ReciterProps {
     reciter: {
         name: string
@@ -14,8 +13,10 @@ function Reciter({ reciter }: ReciterProps) {
     const { name, id, description, image } = reciter
 
     return (
-        <article className="mb-8 flex gap-3 sm:w-96 mx-auto">
-            <img className="rounded w-24 h-24 cursor-pointer" src={image} alt="" />
+        <article className="mb-8 grid grid-flow-col gap-3 sm:w-96 mx-auto">
+            <div className="w-24 h-24 pb-2/4 relative rounded">
+                <img className="w-full h-full absolute object-cover rounded" src={`/src/Assets/Images/${image}`} />
+            </div>
             <main className="text-left align-center">
                 <h6 className="font-bold">{name}</h6>
                 <p className="text-sm">{description}</p>
