92ebdd346e9e979aae1bbe907bfe5b5b40295067
Awad <awad@moblox.com>
2021-10-22 20:08:55 +0100

    Added more reciters

--- src/Data/reciters.ts
+++ src/Data/reciters.ts
@@ -1,19 +1,48 @@
-import mishary from '../Assets/Images/mishary-al-afasy.jpg'
-import assufi from '../Assets/Images/abdurrashid-sufi.jpg'
+import mishary from "../Assets/Images/mishary-al-afasy.jpg";
+import assufi from "../Assets/Images/abdurrashid-sufi.jpg";
+import abdulbasit from "../Assets/Images/abdul-basit.jpg";
+import sudais from "../Assets/Images/abdurrahman-as-sudais.jpg";
+import addussary from "../Assets/Images/yasser-ad-dussary.jpg";
+import huthayfi from "../Assets/Images/huthayfi.jpg";
 
 const reciters = [
-    {
-        "name": "Mishary Al Afasy",
-        "id": "mishaari_raashid_al_3afaasee",
-        "description": "Mishary is an excellent and established Qari with a beautiful recitation style",
-        "image": mishary
-    },
-    {
-        "name": "Abdurrashid As Sufi",
-        "id": "abdurrashid_sufi",
-        "description": "MashaAllah TabarakAllah wonderful reciter",
-        "image": assufi
-    }
-]
+  {
+    name: "Mishary Al Afasy",
+    id: "mishaari_raashid_al_3afaasee",
+    description:
+      "Mishary is an excellent and established Qari with a beautiful recitation style",
+    image: mishary,
+  },
+  {
+    name: "Abdurrashid As Sufi",
+    id: "abdurrashid_sufi",
+    description: "MashaAllah TabarakAllah wonderful reciter",
+    image: assufi,
+  },
+  {
+    name: "Abdul Basit (Murattal)",
+    id: "abdul_basit_murattal",
+    description: "",
+    image: abdulbasit,
+  },
+  {
+    name: "Abdurrahman As Sudays",
+    id: "abdurrahmaan_as-sudays",
+    description: "",
+    image: sudais,
+  },
+  {
+    name: "Yasser Ad Dussary",
+    id: "yasser_ad-dussary",
+    description: "",
+    image: addussary,
+  },
+  {
+    name: "Huthayfi",
+    id: "huthayfi",
+    description: "",
+    image: huthayfi,
+  },
+];
 
-export default reciters
+export default reciters;
